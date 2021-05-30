2ae08cad2c4f56da7021ecfcd3f5e7475af2743c
Vadim Chernov <dimuls@yandex.ru>
2021-05-30 21:58:03 +0300

    save point

--- cmd/explorer/main_fabric_sample.go
+++ cmd/explorer/main_fabric_sample.go
@@ -3,10 +3,28 @@
 package main
 
 import (
+	"explorer"
+	"explorer/hf"
 	_ "explorer/mod/fabric_sample"
 	"explorer/pg"
 )
 
 func run() {
+
+	hf.RegisterJSONValueType("basic", "^asset", "asset")
+
+	pg.RegisterQuery(pg.Query{
+		Name: "assets_by_owner_and_size",
+		Body: `
+			select *
+			from state
+			where type = 'asset'
+				and value#>>'{"owner"}' = $1
+				and (value#>>'{"size"}')::integer < $2
+			order by key asc
+		`,
+		DestElem: &explorer.State{},
+	})
+
 	pg.RunExplorer()
 }
